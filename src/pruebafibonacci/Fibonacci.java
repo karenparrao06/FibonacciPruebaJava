f73b2b6996a389dfc40392b93bb925baba46e1f6
karenparrao06 <karenparrao2015@gmail.com>
2022-06-09 17:01:28 -0500

    TestCeroPassed

--- src/pruebafibonacci/Fibonacci.java
+++ src/pruebafibonacci/Fibonacci.java
@@ -11,6 +11,24 @@ package pruebafibonacci;
  */
 public class Fibonacci {
     
-    
+    public boolean calcularFibonacci(int numero){
+        
+        if (numero <= 0 || numero > 500) {
+            return false;
+        }
+       
+        int a, b, i, auxiliar;
+        a = 0;
+        b = 1;
+
+        for (i = 0; i < numero; i++) {
+            auxiliar = a;
+            a = b;
+            b = auxiliar + a;
+            System.out.println(a);
+        }
+        
+        return true;
+    }
     
 }
